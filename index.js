1e7cf1daa66270260f7c6cae4e06c2d4a1cdeb6d
jay196150 <jay196150@gmail.com>
2023-12-20 20:47:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const {createServer} = require("http")
+
+const {Server} = require( "socket.io" );
+
+const cors = require("cors");
+
+
+const app = express();
+app.use( cors( {origin: "http://localhost:3000"} ) )
+
+const httpServer = createServer( app );
+
+
+const io = new Server( httpServer , {cors : "http://localhost:3000" } )
+
+io.on( "connection" , (socket)=>{
+    console.log( "connected hii" );
+   
+    socket.on( "beginPath" , (arg) => {
+        socket.broadcast.emit( "beginPath" , arg );
+       
+    } )
+
+    socket.on( "drawLine" , (arg) => {
+        socket.broadcast.emit( "drawLine" , arg );
+        
+    } )
+
+    socket.on( "changeConfig" , (arg) => {
+        socket.broadcast.emit( "changeConfig" , arg );
+        console.log( arg )
+        
+    } )
+
+} ) 
+
+
+ 
+
+httpServer.listen(5000)
